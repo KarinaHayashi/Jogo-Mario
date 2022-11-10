1ea60b585a2d62b2accfd09016ca407b28a1c60d
WillHirosh <hiroshi.willian@gmail.com>
2022-11-10 16:01:29 -0300

    Incluindo evento de score e audio na animacao

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,19 @@
 
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const bullet = document.querySelector('.bullet');
+const startGame = document.querySelector('.start-game');
+var cont = document.querySelector('.score-amount');
 
-const jump =() => {
- mario.classList.add('jump');
+var soma = Number(cont.textContent)
+
+
+const jump = () => {
+  mario.classList.add('jump');
+
+  soma += 1
+  cont.textContent = soma
+
 
   setTimeout(() => {
 
@@ -12,29 +22,65 @@ const jump =() => {
   }, 500);
 }
 
+
+
+//funcao do evento dos obstaculos
 const loop = setInterval(() => {
 
-    console.log('loop')
+  //condicao da variavel do cano  
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+  const bulletPosition = bullet.offsetLeft;
+  const pipePosition = pipe.offsetLeft;
 
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    console.log(marioPosition);
+  // verifica a condicao do jogo se o mario encostou no cano
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-     
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+    bullet.style.left = `${bulletPosition}px`;
 
-        mario.style.animation = 'none';
-        mario.style.left = `${marioPosition}px`;
-        
-        mario.src= './imagens/game-over.png';
-        mario.style.width = '75px'
-        mario.style.marginLeft = '50px'
+    mario.style.animation = 'none'; //remove a animacao do mario
+    mario.style.bottom = `${marioPosition}px`;
 
-        clearInterval(loop);
+    document.getElementById('theme').pause();
+    document.getElementById('gameOver').play();
+    mario.src = './imagens/game-over.png';
+    mario.style.width = '75px';
+    mario.style.marginLeft = '50px';
+
+    startGame.src = './imagens/Over.png';
+
+    clearInterval(loop); //para o loop
+
+    document.addEventListener('keydown', () => {
+      location.reload();
+    })
+
+    if (bulletPosition <= 120 && bulletPosition > 0 && marioPosition < 80) {
+
+      //remove a animacao da bala de canhao
+      bullet.style.animation = 'none';
+      //mantem a posicao da bala de canhao
+      bullet.style.left = `${bulletPosition}px`;
+      pipe.style.left = `${pipePosition}px`;
+      //remove a animacao do mario
+      mario.style.animation = `none`;
+      //mantem a animacao do mario;
+      mario.style.bottom = `${marioPosition}px`;
+
+      document.getElementById('theme').pause();
+      document.getElementById('gameOver').play();
+      mario.src = './imagens/marioGameOver.png'
+      mario.style.width = '75px';
+      mario.style.marginLeft = '80px';
+
+      startGame.src = './imagens/Over.png';
+
     }
+
+
+  }
 }, 10);
 
 
